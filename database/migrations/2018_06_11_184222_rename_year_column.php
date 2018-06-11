c3ae8bf47d5499a71a1b28c127575c0d07e09aa4
LAURO\LuisLauro <lauro_silva_ala@hotmail.com>
2018-06-11 13:46:12 -0500

    ano heh

---
+++ database/migrations/2018_06_11_184222_rename_year_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameYearColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salas', function (Blueprint $table) {
+            $table->renameColumn('año', 'ano');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('salas', function (Blueprint $table) {
+            $table->renameColumn('ano', 'año');
+        });
+    }
+}
