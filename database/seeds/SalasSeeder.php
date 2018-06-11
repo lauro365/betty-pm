f3e91a717ec6b2dbb92be940549711678b6e2049
LAURO\LuisLauro <lauro_silva_ala@hotmail.com>
2018-06-11 17:29:47 -0500

    ano heh x7

--- database/seeds/SalasSeeder.php
+++ database/seeds/SalasSeeder.php
@@ -12,26 +12,9 @@ class SalasSeeder extends Seeder
     public function run()
     {
         DB::table('salas')->insert([
-                [
-                    'num_sala' => '1',
-                    'cupo_maximo' => '100',
-                ],
-                [
-                    'num_sala' => '2',
-                    'cupo_maximo' => '75',
-                ],
-                [
-                    'num_sala' => '3',
-                    'cupo_maximo' => '50',
-                ],
-                [
-                    'num_sala' => '4',
-                    'cupo_maximo' => '25',
-                ],
-                [
-                    'num_sala' => '5',
-                    'cupo_maximo' => '10',
-                ]]
+                'num_sala' => 6,
+                'cupo_maximo' => '125',
+            ]
         );
     }
 }
